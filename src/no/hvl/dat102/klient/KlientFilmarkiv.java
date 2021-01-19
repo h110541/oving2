14407301b7172add5b38cc3470fd446ff1a0df6f
h110541 <110541@stud.hvl.no>
2021-01-19 12:34:36 +0100

    Lagt til set-metoder i Film-klassen

--- src/no/hvl/dat102/klient/KlientFilmarkiv.java
+++ src/no/hvl/dat102/klient/KlientFilmarkiv.java
@@ -4,7 +4,7 @@ import no.hvl.dat102.Filmarkiv;
 import no.hvl.dat102.adt.FilmarkivADT;
 
 public class KlientFilmarkiv {
-	
+
 	public static void main(String[] args) {
 		FilmarkivADT fa = new Filmarkiv(10);
 		Meny meny = new Meny(fa);
